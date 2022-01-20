83ca4c1dc1e1e9f0229e1ed244cbedb77b5d5bfa
soaresmilton <eng.milton.soares@gmail.com>
2022-01-20 20:55:58 -0300

    Error handler

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const puppeteer = require('puppeteer');
 const readlineSync = require('readline-sync');
 
-console.log("Bem vindo ao conversor de moedas!")
+console.log("Bem vindo ao conversor de moedas!");
+
+// const currencies = {
+//   'Dólar Americano: ': 'USD',
+//   'Real: ': 'BRL',
+//   'Dólar Canadense: ': 'CAD',
+//   'Euro: ': 'EUR',
+//   'Libras Esterlinas: ': 'GBP',
+//   'Yuan Chinês: ': 'CNY',
+//   'Yen Japonês: ': 'JPY',
+//   'Rupee Indiano: ': 'IDR'
+// };
+
+const currencies = ['AUD', 'BRL', 'CAD', 'CNY', 'EUR', 'GBP', 'IDR', 'INR', 'JPY', 'PHP', 'PLN', 'USD'];
+
+console.log('LISTA DE MOEDAS DISPONÍVEIS PARA CONVERSÃO:');
+
+currencies.forEach(el => console.log(el));
 
 
 async function robo() {
 
-  const referenceCurrency = readlineSync.question('Qual a moeda de referência? ') || 'dolar';
-  const convertedCurrency = readlineSync.question('Para qual moeda deseja converter? ') || 'real';
+  const referenceCurrency = readlineSync.question('Qual a moeda de referência? ') || 'USD';
+  const convertedCurrency = readlineSync.question('Para qual moeda deseja converter? ') || 'BRL';
+
+  const validReferenceCurrency = currencies.includes(referenceCurrency);
+  const validConvertedCurrency = currencies.includes(convertedCurrency);
+
+
+  if (!validReferenceCurrency || !validConvertedCurrency) {
+    console.error('Moeda escolhida inválida. Por favor selecione alguma que esteja na lista acima.')
+    return;
+  };
+
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
